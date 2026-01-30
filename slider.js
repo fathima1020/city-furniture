e7e70ae125638aafed8bdd5778fe53f7f733e6cc
fathima <barvin5713@gmail.com>
2026-01-30 00:31:37 +0000

    added margin

--- slider.js
+++ slider.js
@@ -6,22 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
   slider = document.getElementById("categorySlider");
   leftArrow = document.querySelector(".arrow.left");
   rightArrow = document.querySelector(".arrow.right");
-
-  updateArrows();
 });
 
 function scrollPrev() {
   slider.scrollLeft -= 300;
-  updateArrows();
 }
 
 function scrollNext() {
   slider.scrollLeft += 300;
-  updateArrows();
-}
-
-function updateArrows() {
-  leftArrow.disabled = slider.scrollLeft <= 0;
-  rightArrow.disabled =
-    slider.scrollLeft + slider.clientWidth >= slider.scrollWidth;
 }
